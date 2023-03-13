ae765e67b200f86cc48c9906fa6220456b685118
Omar-Codess <omartaki8@gmail.com>
2023-03-13 14:26:11 +0100

    creato progetto

---
+++ src/org/lessons/java/inheritance/shop/Prodotto.java
@@ -0,0 +1,15 @@
+package org.lessons.java.inheritance.shop;
+
+
+/*
+Creare la classe Prodotto che gestisce i prodotti dello shop.
+        Un prodotto è caratterizzato da:
+        - codice (numero intero)
+        - nome
+        - descrizione
+        - prezzo
+        - iva
+*/
+public class Prodotto {
+
+}
