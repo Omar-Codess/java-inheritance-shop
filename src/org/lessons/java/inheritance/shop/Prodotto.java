cf4f8dcee7587e50e50e00a9c5bafdf2ba963787
Omar-Codess <omartaki8@gmail.com>
2023-03-13 14:47:20 +0100

    creati getter e setter

--- src/org/lessons/java/inheritance/shop/Prodotto.java
+++ src/org/lessons/java/inheritance/shop/Prodotto.java
@@ -11,5 +11,48 @@ public class Prodotto {
     private BigDecimal vat;
 
     //COSTRUTTORE
+    public Prodotto(int code, String name, String description, BigDecimal price, BigDecimal vat) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getVat() {
+        return vat;
+    }
+
+    public void setVat(BigDecimal vat) {
+        this.vat = vat;
+    }
 
 }
