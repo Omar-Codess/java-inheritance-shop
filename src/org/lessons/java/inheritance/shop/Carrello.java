cd2464ed5994c66f5c29805abe72c2a2b112fd20
Omar-Codess <omartaki8@gmail.com>
2023-03-13 16:16:24 +0100

    creato metodo main nella classe carrello

--- src/org/lessons/java/inheritance/shop/Carrello.java
+++ src/org/lessons/java/inheritance/shop/Carrello.java
@@ -1,5 +1,19 @@
 package org.lessons.java.inheritance.shop;
 
-public class Carrello {
+import java.util.Scanner;
 
+public class Carrello {
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Benvenuto! Scegli cosa desideri mettere nel carrello:");
+        System.out.println("- Digita s per Smartphone;");
+        System.out.println("- Digita t per Televisione;");
+        System.out.println("- Digita c per Cuffie;");
+
+        String userInput = scan.nextLine();
+
+
+    }
 }
